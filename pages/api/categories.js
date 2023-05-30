8e3dd4a7654921ae8fb7b85e61b6d24af31162bc
saadkarim402 <saadkarim782@gmail.com>
2023-05-30 09:44:24 -0700

    c7

--- pages/api/categories.js
+++ pages/api/categories.js
@@ -1,17 +1,18 @@
 import { mongooseConnect } from "@/lib/mongoose";
 import { Category } from "@/models/category";
 
-export default async function handle(req,res){
-    const {method}=req;
-    await mongooseConnect();
+export default async function handle(req, res) {
+  const { method } = req;
+  await mongooseConnect();
 
-    if(method==="GET"){
-        res.json ( await Category.find());
-    }
+  if (method === "GET") {
+    res.json(await Category.find().populate('parent'));
+  }
 
-    if(method=='POST'){
-        const {name}=req.body;
-        const categoryDoc=await Category.create({name});
-        res.json(categoryDoc);
-    }
+  if (method == "POST") {
+    const { name, parentCategory } = req.body;
+   // console.log(parentCategory);
+    const categoryDoc = await Category.create({ name, parent: parentCategory, });
+    res.json(categoryDoc);
+  }
 }
--- pages/categories.js
+++ pages/categories.js
@@ -5,6 +5,7 @@ import { useEffect, useState } from "react";
 export default function Categories() {
     const [name,setName]=useState('');
     const [categories,setCategories]=useState('');
+    const [parentCategory,setParentCategory]=useState('');
     
     //for fetching data
     useEffect(()=>{
@@ -20,7 +21,7 @@ export default function Categories() {
     //when saving data 
     async function saveCategory(ev){
         ev.preventDefault();
-        await axios.post('/api/categories',{name});
+        await axios.post('/api/categories',{name,parentCategory});
         setName('');
         fetchCategories();
     }
@@ -34,18 +35,24 @@ export default function Categories() {
           placeholder="Category name"
           onChange={(ev) => setName(ev.target.value)}
           value={name}/>
+          <select className="mb-0" onChange={ev=>setParentCategory(ev.target.value)}>
+          {categories.length>0&&categories.map(it=>(
+            <option value={it._id} >{it.name}</option>))}
+          </select>
         <button className="btn-primary py-1">Save</button>
       </form>
       <table className="basic mt-4">
         <thead>
           <tr>
             <td>Category Name</td>
+            <td>parentCategory</td>
           </tr>
         </thead>
         <tbody>
           {categories.length>0&&categories.map(it=>(
             <tr>
               <td>{it.name}</td>
+              <td>{it?.parent?.name}</td>
             </tr>
           ))}
         </tbody>
